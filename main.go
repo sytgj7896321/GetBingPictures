226170a7e6db971d72c96a7e7a74536a6e2578b0
sytgj7896321 <sytgj7896321@gmail.com>
2021-07-02 01:28:40 +0800

    beta

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ var (
 func main() {
 	flag.IntVar(&goroutines, "c", 4, "Set how many coroutines you want to use like -c 8")
 	flag.StringVar(&fetcher.ProxyAdd, "p", "", "Set http proxy address like -p \"http://127.0.0.1:10809\"")
-	flag.BoolVar(&dailyMode, "daily-mode", false, "set true to open daily update mode")
+	flag.BoolVar(&dailyMode, "daily-mode", true, "set false to open full download mode")
 	flag.Parse()
 
 	var lastNum int
